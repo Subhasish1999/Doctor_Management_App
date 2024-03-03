d66cddd0d0a1b23d7592237b03f5a30a2bfe5ec6
Subhasish1999 <subhasishghosh892@gmail.com>
2024-03-03 15:26:00 +0530

    latest update

--- frontend/src/Components/ProtectedRoutes.js
+++ frontend/src/Components/ProtectedRoutes.js
@@ -1,11 +1,44 @@
-import React from "react"
-import { Navigate } from "react-router-dom"
-import {useSelector, useDispatch} from "react-redux"
+import React, { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
+import { hideLoading, showLoading } from "../Redux/features/alertSlice";
+import axios from "axios";
+import { setUser } from "../Redux/features/userSlice";
+
 
 export default function ProtectedRoutes({children}) {
 
   const dispatch = useDispatch()
-  // const (user) = useSelector(state => state.user)
+  const {user} = useSelector(state => state.user)
+  //get user
+  const getUser = async() => {
+    try {
+      dispatch(showLoading())
+      const res = await axios.post("/api/v1/users/getUser", {token : localStorage.getItem("token")},
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      }
+      )
+      dispatch(hideLoading())
+      if(res.data.success){
+        dispatch(setUser(res.data.data))
+      }else{
+        <Navigate to = "/login"/>
+      }
+    } catch (error) {
+      dispatch(hideLoading())
+      console.log(error);
+    }
+  }
+
+useEffect(() => {
+  if(!user){
+    getUser();
+  }
+}, [user])
+
   if(localStorage.getItem("token")){
     return children
   }else{
